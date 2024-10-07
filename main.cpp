7b8ed647d4f4e1ef4a1f7be4e6520372e8e3f6a6
Chanty91 <chantal.matu@strathmore.edu>
2024-10-07 17:03:18 +0300

    'Fisrt commit'

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <sstream>
+#include <vectors>
+#include <fstream>
+
+using namespace std;
+
+
+struct Fixtures {
+    string hometeam;
+    string awayteam;
+    string hometown;
+    string stadium;
+    int leg;
+    int weekend;
+};
+
+vector<string> team;
+vector<string> town;
+vector<string> stadium;
+vector<Fixtures> fixtures;
+
+int main(){
+    //continue here
+    return 0;
+}
