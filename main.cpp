c5666c48e7891c8366a20f7d99553d80c70ff73d
HSigei <harry.sigei@strathmore.edu>
2024-10-07 17:06:20 +0300

    'read from csv'

--- main.cpp
+++ main.cpp
@@ -21,6 +21,30 @@ vector<string> stadium;
 vector<Fixtures> fixtures;
 
 int main(){
-    //continue here
+    //READING FROM CSV
+    ifstream myFile("teams_file.csv");
+    string line = "";
+
+    while(getline(myFile, line)) {
+        stringstream ss(line);
+        string teamToPush, townToPush, stadiumToPush;
+
+        getline(ss, teamToPush,',');
+        getline(ss, townToPush,',');
+        getline(ss, stadiumToPush,',');
+
+        //to check if the file is being read
+
+        cout<<teamToPush<<",";
+        cout<<townToPush<<",";
+        cout<<stadiumToPush<<endl;
+
+        team.push_back(teamToPush);
+        town.push_back(townToPush);
+        stadium.push_back(stadiumToPush);
+
+    }
+
+    myFile.close();
     return 0;
 }
